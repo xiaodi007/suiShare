6fd40b72833ab50f39a4fa805d4ff271c39710e2
xiaodi <334830452@qq.com>
2025-05-13 15:18:14 +0800

    change walrus pass route

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,8 @@
 export const PACKAGE_ID = "0xccfcd320046e597fef276420c45c434936731e1385bc7cab150924f31aa27f73";
 export const TTL_MIN = 10; // 签名有效期
 
-// const PUBLISH_URL = "https://walrus-publisher.thcloud.dev";
-export const PUBLISH_URL = "http://waltest.chainflow.io:9001"; // 文件上传地址
+export const PUBLISH_URL = "https://walrus-publisher.thcloud.dev";
+// export const PUBLISH_URL = "http://waltest.chainflow.io:9001"; // 文件上传地址
 // const PUBLISH_URL = "https://publisher.testnet.walrus.atalma.io";
 export const AGGREGATOR_URL = "https://aggregator.walrus-testnet.walrus.space"; // 文件预览地址
 
